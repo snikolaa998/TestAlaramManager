9a5584b5c5ad2259cb7e3456ac879b33ad862804
Nikola Savic <snikolaa998@gmail.com>
2020-12-08 20:08:43 +0100

    Receiver

--- app/src/main/java/com/example/testalarammanager/MainActivity.kt
+++ app/src/main/java/com/example/testalarammanager/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.testalarammanager
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Toast
 import android.widget.ToggleButton
 
@@ -23,15 +21,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notifyIntent = Intent(this, MyReceiver::class.java)
+        val notifyPendingIntent = PendingIntent.getBroadcast(
+                this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
         createNotificationChannel()
         val alarmToggle = findViewById<ToggleButton>(R.id.alarmToggle)
         alarmToggle.setOnCheckedChangeListener { buttonView, isChecked ->
             val toastMessage: String
             if (isChecked) {
+                val repeatInterval = 1000 * 180
+                val triggerTime = SystemClock.elapsedRealtime() + repeatInterval
                 toastMessage = "Stand Up Alarm on"
-                deliverNotification(this)
+                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, repeatInterval.toLong(), notifyPendingIntent)
             } else {
-                notificationManager.cancelAll()
+                alarmManager.cancel(notifyPendingIntent)
                 toastMessage = "Stand Up Alarm off"
             }
             Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
@@ -47,20 +53,5 @@ class MainActivity : AppCompatActivity() {
         }
         notificationManager.createNotificationChannel(notificationChannel)
     }
-    private fun deliverNotification(context: Context) {
-        val contentIntent = Intent(context, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-                context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT
-        )
-        val notification = Notification.Builder(context, NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle("Stand Up Alert")
-                .setContentText("You should stand up and walk around now")
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .build()
 
-        notificationManager.notify(NOTIFICATION_ID, notification)
-
-    }
 }
--- app/src/main/java/com/example/testalarammanager/MyReceiver.kt
+++ app/src/main/java/com/example/testalarammanager/MyReceiver.kt
@@ -1,12 +1,36 @@
 package com.example.testalarammanager
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
 class MyReceiver : BroadcastReceiver() {
 
+    private lateinit var notificationManager: NotificationManager
+    private val NOTIFICATION_ID = 0
+    private val NOTIFICATION_CHANNEL_ID = "com.example.testalarmmanager"
+
     override fun onReceive(context: Context, intent: Intent) {
-        
+        notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        deliverNotification(context)
+    }
+    private  fun deliverNotification(context: Context) {
+        val contentIntent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val notification = Notification.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("Stand Up Alert")
+            .setContentText("You should stand up and walk around now")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
+
     }
 }
